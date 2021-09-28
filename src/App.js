2e3c87a2fdb639b791fd599614b5c294a933ce5b
hmntk <hemanth.anits.85@gmail.com>
2021-09-28 23:57:12 +0530

    images upload

--- src/App.js
+++ src/App.js
@@ -5,37 +5,6 @@ import "./Components/Signup";
 function App() {
   return (
     <div className="App">
-
-
-      <h1>This is Tarun's branch</h1>
-      <h1>testing something ...</h1>
-      <h1>again testing...  </h1>
-      <h1>again testing...  </h1>
-      <h1>again testing...  </h1>
-
-
-      <h1>This is Bhawani's branch</h1>
-      <h2>Working fine</h2>
-      <h2>Working fine2</h2>
-      <h1>This is Hemanth's branch</h1>
-
-      <h1>This is test branch</h1>
-      <span>THis is chnage made by Hemanth</span>
-
-
-      <h1> Final change on test branch at Bhawani</h1>demo1
-      <h1>Change inside new demo branch Bhawani</h1>
-      <h2>This again change from Bhawani Demo1</h2>
-      <h2>This again change from Bhawani Demo1 again</h2>
-
-
-    
-      
-      <h1>Hi this from Abhay branch</h1>
-
-      <h1>This is Abhay's branch</h1>
-      <h2>Prime</h2>
-
     </div>
   );
 }
